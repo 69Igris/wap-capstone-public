8c7a9983d2dfed79fbf8be5691e5e2c259834143
Aaryan Yadav <coder69.igris@gmail.com>
2025-05-08 12:01:51 +0530

    Added Pages

---
+++ src/Pages/ContactUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Contactus from '../components/cotact/Contactus'
+const ContactUs = () => {
+  return (
+    <div>
+      <Contactus/>
+    </div>
+  )
+}
+
+export default ContactUs
